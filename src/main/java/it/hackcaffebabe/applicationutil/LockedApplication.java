c7fafe194ae3930d920947f18b3a890f60be8c0d
AndreaGhizzoni <andrea.ghz@gmail.com>
2016-03-08 10:49:38 +0100

    added preliminary implementation

--- src/main/java/it/hackcaffebabe/applicationutil/LockedApplication.java
+++ src/main/java/it/hackcaffebabe/applicationutil/LockedApplication.java
@@ -1,28 +1,76 @@
 package it.hackcaffebabe.applicationutil;
 
+import java.io.File;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+import java.nio.channels.OverlappingFileLockException;
+
 /**
  * TODO add doc and examples
  */
 public class LockedApplication
 {
-    private static LockedApplication instance;
+    // static constant that returns the platform dependent temporary directory.
+    private static final String TMP_DIR = System.getProperty("java.io.tmpdir");
+    private static final String SEP = System.getProperty("file.separator");
+
+    private File applicationFile;
+    private FileChannel applicationFileChannel;
+    private FileLock applicationFileLock;
+    private String fileID;
 
     /**
      * TODO add doc
-     * @return
+     * @param fileID
      */
-    public static LockedApplication getInstance(){
-        if( instance == null )
-            instance = new LockedApplication();
-        return instance;
+    public LockedApplication(String fileID) {
+        this.setFileID(fileID);
     }
 
-    /*
-     *
-     */
-    private LockedApplication(){
+    public boolean isAlreadyRunning(){
+        try{
+            // get the current process id
+            long pid = Util.getProcessID();
+            // create a file in <TEMP_DIR>/<pid>.lock
+            this.applicationFile = new File( TMP_DIR+SEP+pid+".lock" );
+            this.applicationFileChannel = new RandomAccessFile(
+                    this.applicationFile,
+                    "rw"
+            ).getChannel();
 
+            try {
+                this.applicationFileLock = this.applicationFileChannel.lock();
+            }catch (OverlappingFileLockException ignored){
+                //close lock
+                return true;
+            }
+
+            Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    // closelock
+                    // delete file
+                }
+            }));
+
+
+        }catch (Exception e){
+            //close lock
+            return true;
+        }
+
+        return false;
     }
 
-
+    /**
+     * TODO add doc
+     * @param fileID
+     * @throws IllegalArgumentException
+     */
+    public void setFileID(String fileID) throws IllegalArgumentException {
+        if( fileID == null || fileID.isEmpty() )
+            throw new IllegalArgumentException("FileID given can not be null or empty string");
+        this.fileID = fileID;
+    }
 }
